1b93f539b9d7ff494b657ef9f90042c4b0d73270
jcizel <jcizel@gmail.com>
2016-09-07 16:00:37 +0200

    First commit.

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+##' @import data.table pipeR rlist
+NULL
+
+
+.onLoad <- function(libname = find.package("texutils"), pkgname = "texutils") {
+  message(rep("*",70))
+  message("est2tab")
+  message("By Janko Cizel, 2016. All rights reserved.")
+  message("www.jankocizel.com")
+  message(rep("*",70))
+}
